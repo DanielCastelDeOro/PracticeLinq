2a7c1838962cd44ecccb4a5649b649e9e7288dd6
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-25 10:10:40 -0600

    Commit 1 with questions to John

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 
 
 namespace PracticeLinq
@@ -9,10 +10,13 @@ namespace PracticeLinq
     {
         static void Main(string[] args)
         {
-          
+            //(Using Method Syntax)
+           //Create a list of video game names..Order the list of games by length of the game name.
+           //Use the lambda expression in this exercise as well.
+
             var videoGame = new List<string>() { "Halo", "GTA", "Sonic" };
-            var sorted = videoGame.OrderBy(game => game).ToList();
-            Console.WriteLine(videoGame);
+            var sorted = videoGame.OrderBy(g => g); // Im stuck and dont know how to check to see if im correct?!!?
+            Console.WriteLine(videoGame); // videoGame or sorted wont print for me?
 
         }
     }
