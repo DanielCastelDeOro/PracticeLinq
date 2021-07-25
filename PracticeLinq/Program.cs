2859add38efb32ca9055e2926e0f93c84e735201
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-25 09:54:31 -0600

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var videoGame = new List<string>() { "Halo", "GTA", "Sonic" };
+            var sorted = videoGame.OrderBy(game => game).ToList();
+            Console.WriteLine(videoGame);
+
+        }
+    }
+}
